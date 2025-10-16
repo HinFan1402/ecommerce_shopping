47f5da3a5ccd12fc739aa3ecf4a015a025feb6e7
aHinf1402 <phanhienhd1402@gmail.com>
2025-10-16 10:33:28 +0700

    add function for Admin Areas

---
+++ Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,103 @@
+﻿using ecommerce_shopping.Models;
+using ecommerce_shopping.Repository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce_shopping.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public BrandController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Brands.OrderByDescending(b => b.Id).ToListAsync());
+        }
+    
+        public async Task<IActionResult> Edit(int id)
+        {
+            BrandModel brand = await _dataContext.Brands.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            return View(brand);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int Id, BrandModel brand)
+        {
+            if (ModelState.IsValid)
+            {
+                brand.Slug = brand.Name.Replace(" ", "-");
+                _dataContext.Update(brand);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Update Brand successfull";
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                TempData["error"] = "Model has something wrong!";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+            return View(brand);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(int Id, BrandModel brand)
+        {
+            if (ModelState.IsValid)
+            {
+                brand.Slug = brand.Name.Replace(" ", "-");
+                _dataContext.Add(brand);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Update Brand successfull";
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                TempData["error"] = "Model has something wrong!";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+            return View(brand);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            BrandModel brands = await _dataContext.Brands.FindAsync(id);
+            _dataContext.Remove(brands);
+            await _dataContext.SaveChangesAsync();
+            TempData["success"] = "Delete product successfull";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/CagetoryController.cs
@@ -0,0 +1,101 @@
+﻿using ecommerce_shopping.Models;
+using ecommerce_shopping.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce_shopping.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public CategoryController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Categories.OrderByDescending(b => b.Id).ToListAsync());
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            CategoryModel category = await _dataContext.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int Id, CategoryModel category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.Slug = category.Name.Replace(" ", "-");
+                _dataContext.Update(category);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Update Category successfull";
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                TempData["error"] = "Model has something wrong!";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(int Id, CategoryModel category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.Slug = category.Name.Replace(" ", "-");
+                _dataContext.Add(category);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Update Category successfull";
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                TempData["error"] = "Model has something wrong!";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+            return View(category);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            CategoryModel category = await _dataContext.Categories.FindAsync(id);
+            _dataContext.Remove(category);
+            await _dataContext.SaveChangesAsync();
+            TempData["success"] = "Delete product successfull";
+            return RedirectToAction("Index");
+        }
+    }
+}
